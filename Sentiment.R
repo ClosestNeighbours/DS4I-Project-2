6aed3e3cbba7e94bb0b96d950fc28e3a957c4769
Susana Maganga <susanamaganga@gmail.com>
2023-10-08 22:22:10 +0200

    Loaded packages and data

---
+++ Sentiment.R
@@ -0,0 +1,10 @@
+# Loading packages
+library(tidyverse)
+library(tidytext)
+library(textdata) 
+library(stringr)
+library(lubridate)
+library(devtools)
+
+# Load data
+source("https://raw.githubusercontent.com/ClosestNeighbours/DS4I-Project-2/main/sona-first-steps%20(1).R")
