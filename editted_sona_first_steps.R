9343f5dd217c364f9ce15aeac098a3daa40bd287
pujapande2 <pjpande@gmail.com>
2023-10-15 17:04:18 +0200

    making sure it works

--- editted_sona_first_steps.R
+++ editted_sona_first_steps.R
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-@@ -0,0 +1,74 @@
-  
-  
-=======
->>>>>>> 7065f9c6f8b29edc50cd333326280bac6e1c036a
-  # read in text data files and organise these into a data frame
-  filenames <- c('1994_post_elections_Mandela.txt', '1994_pre_elections_deKlerk.txt', '1995_Mandela.txt', '1996_Mandela.txt', '1997_Mandela.txt', '1998_Mandela.txt', 
+# read in text data files and organise these into a data frame
+filenames <- c('1994_post_elections_Mandela.txt', '1994_pre_elections_deKlerk.txt', '1995_Mandela.txt', '1996_Mandela.txt', '1997_Mandela.txt', '1998_Mandela.txt', 
                  '1999_post_elections_Mandela.txt', '1999_pre_elections_Mandela.txt', '2000_Mbeki.txt', '2001_Mbeki.txt', '2002_Mbeki.txt', '2003_Mbeki.txt', 
                  '2004_post_elections_Mbeki.txt', '2004_pre_elections_Mbeki.txt', '2005_Mbeki.txt', '2006_Mbeki.txt', '2007_Mbeki.txt', '2008_Mbeki.txt', 
                  '2009_post_elections_Zuma.txt', '2009_pre_elections_ Motlanthe.txt', '2010_Zuma.txt', '2011_Zuma.txt', '2012_Zuma.txt', '2013_Zuma.txt', 
